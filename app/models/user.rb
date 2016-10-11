ead3dd6f6756df761755f742096c8d1a19cd2ec2
Gowtham Gopalakrishnan <gowthamgts12@gmail.com>
2016-10-11 23:32:30 +0530

    removed email confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,16 @@
 class User < ActiveRecord::Base
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable,
-          :confirmable
+          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
 
   has_many :todos, dependent: :destroy
 
-  def confirmation_required?
-    !confirmed?
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
   end
 end
